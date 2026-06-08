2bd3bb72c7270de874c5a7d1a0e6b51b8bf36654
Suraj S P <161311066+DSATM1@users.noreply.github.com>
2026-06-08 05:42:37 +0000

    Refactor list_basics.py to include example lists and print statements

--- list_basics.py
+++ list_basics.py
@@ -1,1 +1,9 @@
-print("Welcome to List")
+"""print("Welcome to List")
+
+marks = [90,20,45,67,89]
+name = ["Suraj", "Putta", "Balu"]
+mix = [30,"Suraj",3.14,True]
+empty = []
+print(len(marks))
+print(mix)
+print(type(name))"""
